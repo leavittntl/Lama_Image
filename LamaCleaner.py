591492e099efb0a2fcee4a9f5dfe7770fd72c546
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-11-14 12:44:24 -0500

    Create Main and Req

---
+++ LamaCleaner.py
@@ -0,0 +1,13 @@
+import streamlit as st
+# import necessary components from cloned repository
+
+
+def main():
+    st.header("Lama Cleaner")
+
+    # Set up Streamlit interface
+    # Accept inputs, call model, display outputs
+    
+
+if __name__ == "__main__":
+    main()
